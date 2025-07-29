b94d022e153f107c0d4ee2a78c93f5155b94b9c5
Vikrant <vgandhekar05@gmail.com>
2025-07-29 21:43:04 +0530

    env update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,9 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+);
 
 export default supabase;
-export { supabaseUrl, supabaseKey };

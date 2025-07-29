f1422c212447a0373717291f8848662c9bed32dc
Vikrant <vgandhekar05@gmail.com>
2025-07-29 21:42:01 +0530

    env update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -6,4 +6,4 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
-export { supabaseUrl, supabaseServiceKey };
+export { supabaseUrl, supabaseKey };

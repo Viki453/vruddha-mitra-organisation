72f39220cebfa03101323dfd1081ec2c5db14419
Vikrant <vgandhekar05@gmail.com>
2025-07-29 21:32:11 +0530

    env update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hsgefnzqzmubkazvxyqw.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZ2Vmbnpxem11YmthenZ4eXF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDk5NzksImV4cCI6MjA2NDYyNTk3OX0.zcb0Ax0tpmSgIwHQOs59VGzz5vSlDsU4uWEr0-Uqpl8";
-const supabaseServiceKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZ2Vmbnpxem11YmthenZ4eXF3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0OTA0OTk3OSwiZXhwIjoyMDY0NjI1OTc5fQ.CZtfzKZL85k4pgisk8HBz484Pcqh_jXjGuaXDHuHr0c";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseServiceKey = import.meta.env.VITE_SUPABASE_SERVICE_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
+
 export default supabase;
 export { supabaseUrl, supabaseServiceKey };

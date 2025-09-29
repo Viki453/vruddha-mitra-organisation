7db9e5822a47c9a7efc7db085df8043473fa09eb
Vikrant <vgandhekar05@gmail.com>
2025-09-29 19:26:58 +0530

    API KEYS CHANGE

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hsgefnzqzmubkazvxyqw.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhzZ2Vmbnpxem11YmthenZ4eXF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDk5NzksImV4cCI6MjA2NDYyNTk3OX0.zcb0Ax0tpmSgIwHQOs59VGzz5vSlDsU4uWEr0-Uqpl8";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseServiceKey = import.meta.env.VITE_SUPABASE_SERVICE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
-export { supabaseUrl, supabaseKey };
+export { supabaseUrl, supabaseServiceKey };
